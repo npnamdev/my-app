0b877b5f2948831342d798258838a23e0271eb88
Phuong Nam <npnam031101@gmail.com>
2022-12-03 03:36:45 -0500

    video 16

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Phương Nam
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AppComponent</h1>
+      <MyComponent />
     </div>
   );
 }
---
+++ src/components/MyComponents.js
@@ -0,0 +1,10 @@
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <h1>MyComponent {Math.random()}</h1>
+        )
+    }
+}
+
+export default MyComponent;
